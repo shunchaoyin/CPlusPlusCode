0361a2f74dd9c01a918e6dd4035e4ba552625506
Shunchaoyin <shunchaoyin@gmail.com>
2024-06-13 23:05:07 +0800

    格式修改与文件汇总启动

--- CPluslog/utility/Logger.h
+++ CPluslog/utility/Logger.h
@@ -4,8 +4,10 @@
 #include <fstream>
 using namespace std;
 
-namespace yazi {
-namespace utility {
+namespace yazi
+{
+    namespace utility
+    {
 
 #define debug(format, ...) \
     Logger::instance()->log(Logger::DEBUG, __FILE__, __LINE__, format, ##__VA_ARGS__)
@@ -22,40 +24,40 @@ namespace utility {
 #define fatal(format, ...) \
     Logger::instance()->log(Logger::FATAL, __FILE__, __LINE__, format, ##__VA_ARGS__)
 
+        class Logger
+        {
+        public:
+            enum Level
+            {
+                DEBUG = 0,
+                INFO,
+                WARN,
+                ERROR,
+                FATAL,
+                LEVEL_COUNT
+            };
 
-class Logger
-{
-public:
-    enum Level
-    {
-        DEBUG = 0,
-        INFO,
-        WARN,
-        ERROR,
-        FATAL,
-        LEVEL_COUNT
-    };
+            static Logger *instance();
+            void open(const string &filename);
+            void close();
+            void log(Level level, const char *file, int line, const char *format, ...);
+            void max(int bytes);
+            void level(int level);
 
-    static Logger* instance();
-    void open(const string &filename);
-    void close();
-    void log(Level level, const char* file, int line, const char* format, ...);
-    void max(int bytes);
-    void level(int level);
+        private:
+            Logger();
+            ~Logger();
+            void rotate();
 
-private:
-    Logger();
-    ~Logger();
-    void rotate();
+        private:
+            string m_filename;
+            ofstream m_fout;
+            int m_max;
+            int m_len;
+            int m_level;
+            static const char *s_level[LEVEL_COUNT];
+            static Logger *m_instance;
+        };
 
-private:
-    string m_filename;
-    ofstream m_fout;
-    int m_max;
-    int m_len;
-    int m_level;
-    static const char* s_level[LEVEL_COUNT];
-    static Logger *m_instance;
-};
-
-}}
+    }
+}
